169dae04d601ee41a377fc2cb2a16b6d65c73652
Dmitry Smolyakov <mompizzapls@mail.ru>
2018-04-13 10:55:57 +0300

    Long-poll working!

--- app/app.php
+++ app/app.php
@@ -2,6 +2,8 @@
 
 require '../vendor/autoload.php';
 
+use Amp\Artax\Request;
+use Amp\Artax\Response;
 use TutuBot\TelegramType\APIResponse;
 use Weasel\WeaselDoctrineAnnotationDrivenFactory;
 
@@ -12,43 +14,106 @@ $_ = new APIResponse();
 const TOKEN = '432585264:AAH_6U4kCYdY-dcbcXz04TxVh80j-IgkQQo';
 const PING = 'https://api.telegram.org/bot' . TOKEN . '/getMe';
 const GET_MESSAGE_METHOD = 'https://api.telegram.org/bot' . TOKEN . '/getUpdates';
+const SEND_MESSAGE = 'https://api.telegram.org/bot' . TOKEN . '/sendMessage';
 const HTTPBIN = 'https://httpbin.org/get';
 
 $factory = new WeaselDoctrineAnnotationDrivenFactory();
 $mapper = $factory->getJsonMapperInstance();
-//$thing = $mapper->readString($json, $typeToDecodeTo);
+$rawType = new \TutuBot\JsonMarshaller\Type\RawType();
+$mapper->registerJsonType('raw', $rawType);
+$bot = new \TutuBot\BotAPI($client, $mapper);
+\Amp\Loop::run(function () use ($bot) {
+    while (true) {
 
-\Amp\Loop::run(function () use ($client, $mapper) {
 
+        /** @var \TutuBot\TelegramType\Update[] $updates */
+        while ($updates = yield $bot->getUpdates()) {
+            foreach ($updates as $update) {
+                var_dump($update);
+                $bot->sendMessage($update->getMessage()->getFrom()->getId(), "Answer!", $update->getMessage()->getId());
+            }
+            sleep(1);
+        }
+    }
 
-    while ($response = yield $client->request(GET_MESSAGE_METHOD)) {
-        handle($response, $mapper);
-//        var_dump($response->getStatus());
-//        var_dump($response->getHeaders());
+//    $promise = Amp\call(function () use ($client) {
+//        try {
+//            // Yield control until the generator resolves
+//            // and return its eventual result.
+//            $response = yield $client->request("https://example.com/");
 //
-//// Response::getBody() returns a Message
-//// See http://amphp.org/byte-stream/message
+//            $body = yield $response->getBody();
+//
+//            return $body;
+//        } catch (HttpException $e) {
+//            // If promise resolution fails the exception is
+//            // thrown back to us and we handle it as needed.
+//        }
+//    });
+//
+//    while ($_ = yield $promise){
+//        var_dump($_);
+//
+//    }
+
+//    while ($response = yield $client->request(GET_MESSAGE_METHOD)) {
+//        /** @var APIResponse $updates */
+//        while ($updates = yield handle($response, $mapper)) {
+//            if ($updates->getOk()) {
+//                var_dump($updates->getResult());
+//                exit;
+////                var_dump($updates->getResult()[0]);exit;
+//                /** @var Response $res */
+////                $res = yield $client->request(SEND_MESSAGE . '?chat_id=' . $updates->getResult()[0]->getMessage()->getFrom()->getId() . '&text=' . "Hello");
+////                $body = yield $res->getBody();
+//////                var_dump(\json_decode($body)->result);
+////                \var_dump($mapper->readString(\json_encode(\json_decode($body)->result), \TutuBot\TelegramType\Message::class));
+////                exit;
+//            }
+//
+//            /** @var \TutuBot\TelegramType\Update $update */
+////            foreach ($updates->getResult() as $update) {
+//////                \var_dump($update);
+////                $response = yield $client->request(SEND_MESSAGE . '?chat_id=' . $update->getMessage()->getFrom()->getId() . '&text=' . "Hello");
+////
+////            }
+////            sleep(1);
+//        }
+
+
 //        var_dump(yield $response->getBody());
 
-//        sleep(1 / 30);
-        sleep(1);
-    }
+//        sleep(3);
+//    }
 
 });
 
-function handle($response, \Weasel\JsonMarshaller\JsonMapper $mapper)
-{
-    Amp\asyncCall(function () use ($response, $mapper) {
-//        var_dump(\json_decode(yield $response->getBody())->result[0]->message);
-//        exit;
-//        $thing = $mapper->readString(yield $response->getBody(), APIResponse::class);
-//        var_dump($thing);
-//        exit;
-                var_dump(\json_decode(yield $response->getBody())->result[0]->message);
 
-        $thing = $mapper->readString(\json_encode(\json_decode(yield $response->getBody())->result[0]->message), \TutuBot\TelegramType\Message::class);
-        var_dump($thing);
-        exit;
-        sleep(1);
+function handle(Response $response, \Weasel\JsonMarshaller\JsonMapper $mapper)
+{
+    return Amp\call(function (\Weasel\JsonMarshaller\JsonMapper $mapper, $response) {
+        $body = yield $response->getBody();
+//        var_dump($body);
+        return $mapper->readString($body, APIResponse::class);
+    }, $mapper, $response);
+
+
+}
+
+function getUpdates()
+{
+
+}
+
+function sendMessage(\Amp\Artax\Client $client, $chatId, $text)
+{
+    Amp\call(function () use ($client, $chatId, $text) {
+
+        /** @var Response $response */
+        $response = yield $client->request(SEND_MESSAGE . '?chat_id=' . $chatId . '&text=' . $text);
+
+        $body = yield $response->getBody();
+
+        return $body;
     });
 }
--- src/BotAPI.php
+++ src/BotAPI.php
@@ -6,7 +6,107 @@ declare(strict_types=1);
 namespace TutuBot;
 
 
+use Amp\Artax\Client;
+use Amp\Artax\Response;
+use Amp\Promise;
+use JsonSchema\Exception\InvalidArgumentException;
+use TutuBot\TelegramType\APIResponse;
+use TutuBot\TelegramType\Update;
+use Weasel\JsonMarshaller\JsonMapper;
+use function \Amp\call;
+
 class BotAPI
 {
+    /**
+     * @var Client
+     */
+    protected $client;
 
+    /**
+     * @var JsonMapper
+     */
+    protected $mapper;
+
+    /**
+     * @var int
+     */
+    protected $lastId;
+
+    public function __construct(Client $client, JsonMapper $mapper)
+    {
+        $this->client = $client;
+        $this->mapper = $mapper;
+        $this->lastId = 0;
+    }
+
+    public function getUpdates(): Promise
+    {
+        return call(function () {
+            try {
+                $url = GET_MESSAGE_METHOD . ($this->lastId ? '?offset=' . ($this->lastId + 1) : null);
+                \var_dump($url);
+                /** @var Response $response */
+                $response = yield $this->client->request($url);
+
+                /**
+                 * @var APIResponse $apiResponse
+                 */
+                $apiResponse = $this->mapper->readString(yield $response->getBody(), APIResponse::class);
+
+                /**
+                 * Костыль.
+                 * Телеграм отпраляет в поле result как еденичное значение так и МАССИВ значений,
+                 * что ломает маршалинг нахуй, т.к нельзя указать тип (единичное значение| массив значений) Update|Updates[]
+                 */
+                $result = \json_decode($apiResponse->getResult());
+
+
+                if (\is_array($result)) {
+                    /**
+                     * @var Update[] $updates
+                     */
+                    $updates = \array_map(function ($item) {
+                        return $this->mapper->readString(\json_encode($item), Update::class);
+                    }, $result);
+
+                    $this->lastId = $this->getLastUpdateId($updates);
+
+                    return $updates;
+                }
+//                \var_dump('Hello', $result);
+                /**
+                 * @var Update[] $update
+                 */
+                $update = [$this->mapper->readString($result, Update::class)];
+                $this->lastId = $this->getLastUpdateId($update);
+                return $update;
+            } catch (\InvalidArgumentException $exception) {
+                --$this->lastId;
+                //@todo тут чет придумать надо получе
+//                sprintf('%s \n %s', $exception->getMessage(), $response->getBody());
+                return [];
+            }
+        });
+    }
+
+    public function sendMessage($chatId, $text, $reply_to_message_id = null): Promise
+    {
+        return call(function () use ($chatId, $text, $reply_to_message_id) {
+
+            /** @var Response $response */
+            $response = yield $this->client->request(SEND_MESSAGE . '?chat_id=' . $chatId . '&text=' . $text . '&reply_to_message_id=' . $reply_to_message_id);
+
+            return yield $response->getBody();
+        });
+    }
+
+
+    /**
+     * @param Update[] $updates
+     * @return int
+     */
+    protected function getLastUpdateId(array $updates): int
+    {
+        return ($_ = $updates[\count($updates) - 1] ?? null) ? $_->getId() : 0;
+    }
 }
